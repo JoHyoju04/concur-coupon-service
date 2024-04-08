0ac3d0d946d615cb6acc60a456317ac12a3daa71
JoHyoju04 <whgywn369@gmail.com>
2024-04-08 18:35:09 +0900

    feat : 초기 설정 추가

---
+++ src/main/java/johyoju04/concurcouponservice/ConcurCouponServiceApplication.java
@@ -0,0 +1,13 @@
+package johyoju04.concurcouponservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurCouponServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurCouponServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/johyoju04/concurcouponservice/ConcurCouponServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package johyoju04.concurcouponservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurCouponServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
